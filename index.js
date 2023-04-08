d531fd51d07e64ee3e82ff55b5b6c719b3f83737
Prem Sagar <psagar172@gmail.com>
2023-04-08 11:45:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express");
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("The server is running!")
+})
+
+const port = process.env.PORT || process.env.PORT_ON_LOCALHOST
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
